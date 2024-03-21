aa3636362368036a2a226412b6e929969f635abd
Blaise <blaiseaganzepascal10@gmail.com>
2024-03-21 06:26:58 -0700

    Create project item

--- src/components/ProjectComponents.vue
+++ src/components/ProjectComponents.vue
@@ -5,7 +5,9 @@
         <div class="titles">
             project title
         </div>
-        <div class="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad perspiciat</div>
+        <div class="descript">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad perspiciat
+        </div>
         <div class="language">
             <div class="langue">PHP</div>
             <div class="langue">js</div>
@@ -30,23 +32,49 @@
   
   <style>
   .project{
-    width: 100%;
+    width: 80%;
     height: 400px;
-    background-color: red;
     display: inline-flex;
     height: 300px;
-    
-    
+    margin-left: 10%;
+    border-radius: 10px;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+    background-color: #E6E6E6;
+    font-family: Verdana;
   }
   .projectimage{
     width: 30%;
     height:300px;
     background-color: blue;
+    border-radius: 10px;
   }
   .projectdescrp{
     width: 70%;
     height:300px;
-    background-color: yellow;
+    border-radius: 10px;
+    padding: 10px;
+  }
+  .titles{
+    font-size: 25px;
+    margin-bottom: 10px;
+  }
+  .language{
+    width: 100%;
+    display: inline-flex;
+    padding-top: 20px;
+  }
+  .langue{
+    padding-top: 5px;
+    padding-bottom: 5px;
+    padding-left: 10px;
+    padding-right: 10px;
+    border: 2px solid #ABABAB;
+    border-radius: 5px;
+    margin-right: 10px;
+  }
+  .descript{
+    color: #3B3B3B;
+    font-size: 16px;
   }
   
   </style>
